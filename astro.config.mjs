2e3371d039a63d19235d1f1ba2933d8a5346f363
Geoffrey-Pliez <geoffrey.pliez@crem.be>
2025-03-11 17:42:48 +0100

    Ajout de la prise en charge du PWA avec un manifest, intégration d'un service worker et d'un bouton d'installation, ainsi que la fonctionnalité d'exportation des résultats en CSV.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,40 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import serviceWorker from 'astrojs-service-worker';
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    serviceWorker({
+      workbox: {
+        // Mise en cache des ressources statiques
+        runtimeCaching: [
+          {
+            urlPattern: /\.(js|css|png|jpg|jpeg|svg|mp3)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'static-resources'
+            }
+          },
+          {
+            // Cache pour les données d'application (tests, progress, etc)
+            urlPattern: /\/(tests|progress)/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'app-data',
+              networkTimeoutSeconds: 3
+            }
+          },
+          {
+            // Stratégie par défaut pour le reste
+            urlPattern: /.*/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'others'
+            }
+          }
+        ]
+      }
+    })
+  ]
 });
