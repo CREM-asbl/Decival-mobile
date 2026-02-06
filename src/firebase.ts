5e130cec07ae77cc78057caa1d3dbde7b27436b4
Geoffrey-Pliez <geoffrey.pliez@crem.be>
2026-02-06 10:48:25 +0100

    feat: Implement Firebase Analytics and establish a new base Astro layout with PWA and theme support.

---
+++ src/firebase.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+    authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID || "decival",
+    storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
+    measurementId: import.meta.env.PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Analytics only in the browser and if supported
+export const initAnalytics = async () => {
+    if (typeof window !== "undefined" && (await isSupported())) {
+        return getAnalytics(app);
+    }
+    return null;
+};
